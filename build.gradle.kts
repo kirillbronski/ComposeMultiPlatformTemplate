d3a81f298378cf00a5fd86c15faaac1d3d681728
Kirill <kirillbronski@gmail.com>
2023-07-22 23:30:27 +0300

    update gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+buildscript {
+    dependencies {
+        //classpath("com.android.tools.build:gradle:8.0.2")
+        //classpath("com.squareup.sqldelight:gradle-plugin:1.5.5")
+        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.17.3")
+    }
+}
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
@@ -5,4 +12,8 @@ plugins {
     id("com.android.application").apply(false)
     id("com.android.library").apply(false)
     id("org.jetbrains.compose").apply(false)
+}
+
+allprojects {
+    apply(plugin = "kotlinx-atomicfu")
 }
